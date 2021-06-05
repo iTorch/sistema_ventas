63aa170a64f67084ad8d48999daafbf7f4c1f3b8
iTorch <3518110531@uth.edu.mx>
2021-06-05 14:29:30 -0500

    First commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\xxanc\Desktop\Python Projects\Django\Django3\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
